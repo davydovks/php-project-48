485bbf321b7b6d313303ec69d709f23727cac760
Konstantin Davydov <davydovks@mail.ru>
2023-04-27 20:47:34 +0500

    create ShowDoc.php

---
+++ src/ShowDoc.php
@@ -0,0 +1,12 @@
+<?php
+
+$doc = <<<DOC
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+
+DOC;
