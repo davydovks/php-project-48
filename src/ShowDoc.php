201f7736af5202a30ae26b0e8ec2b4f8672ac6a0
Konstantin Davydov <davydovks@mail.ru>
2023-05-02 11:08:19 +0500

    add option --formar

--- src/ShowDoc.php
+++ src/ShowDoc.php
@@ -10,14 +10,16 @@ function getDoc()
 {
     $doc = <<<DOC
     Generate diff
-    
+
     Usage:
       gendiff (-h|--help)
       gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
     
     Options:
       -h --help                     Show this screen
       -v --version                  Show version
+      --format <fmt>                Report format [default: stylish]
     
     DOC;
 
