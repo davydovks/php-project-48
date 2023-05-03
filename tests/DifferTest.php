24630cae2b418d8623c503b59b2e500d02f2015b
Konstantin Davydov <davydovks@mail.ru>
2023-05-03 09:30:40 +0500

    add DifferTest.php

---
+++ tests/DifferTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Differ\Differ\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Differ\Differ\genDiff;
+
+class DifferTest extends TestCase
+{
+    public function testGenDiff(): void
+    {
+        
+    }
+}
