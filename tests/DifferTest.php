aa5160488cae971d53b3a60db6075f3dd25a95dc
Konstantin Davydov <davydovks@mail.ru>
2023-05-03 10:03:46 +0500

    add testGenDiff to DifferTest.php

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -7,8 +7,19 @@ use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
+    public function getFixtureFullPath($fixtureName)
+    {
+        $parts = [__DIR__, 'fixtures', $fixtureName];
+        return realpath(implode('/', $parts));
+    }
+
     public function testGenDiff(): void
     {
-        
+        $JSONFile1 = $this->getFixtureFullPath('file1.json');
+        $JSONFile2 = $this->getFixtureFullPath('file2.json');
+        $expected = $this->getFixtureFullPath('output.txt');
+        $actual = genDiff($JSONFile1, $JSONFile2);
+
+        $this->assertStringEqualsFile($expected, $actual);
     }
 }
