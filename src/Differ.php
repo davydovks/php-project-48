e6baaf8944d758749c526df734350e61a89b910f
Konstantin Davydov <davydovks@mail.ru>
2023-05-24 16:59:17 +0500

    update genDiff to return array, add interface functions

--- src/Differ.php
+++ src/Differ.php
@@ -4,32 +4,6 @@ namespace Differ\Differ;
 
 use function Differ\Parsers\readFile;
 
-function compareArrays(array $arr1, array $arr2, string $key, int $level = 1)
-{
-    $indent = str_repeat("  ", $level);
-    $keyExistsInFile1 = array_key_exists($key, $arr1);
-    $keyExistsInFile2 = array_key_exists($key, $arr2);
-    if ($keyExistsInFile1) {
-        $value1 = json_encode($arr1[$key]);
-    }
-    if ($keyExistsInFile2) {
-        $value2 = json_encode($arr2[$key]);
-    }
-    if ($keyExistsInFile1 && $keyExistsInFile2) {
-        if ($arr1[$key] === $arr2[$key]) {
-            return $indent . "  {$key}: {$value1}\n";
-        } else {
-            $result = $indent . "- {$key}: {$value1}\n";
-            $result .= $indent . "+ {$key}: {$value2}\n";
-            return $result;
-        }
-    } elseif ($keyExistsInFile1) {
-        return $indent . "- {$key}: {$value1}\n";
-    } else {
-        return $indent . "+ {$key}: {$value2}\n";
-    }
-}
-
 function getKeys(mixed $arr)
 {
     return is_object($arr) ? get_object_vars($arr) : array_keys($arr);
@@ -42,17 +16,92 @@ function mergeKeys(mixed $arr1, mixed $arr2)
     return $keys;
 }
 
+function bothHaveArraysByKey(array $arr1, array $arr2, $key)
+{
+    $firstIsArray = isset($arr1[$key]) && is_array($arr1[$key]);
+    $secondIsArray = isset($arr2[$key]) && is_array($arr2[$key]);
+    return $firstIsArray && $secondIsArray;
+}
+
+function createDiff(array $arrayFirst, array $arraySecond)
+{
+    $keys = mergeKeys($arrayFirst, $arraySecond);
+    return array_map(function ($key) use ($arrayFirst, $arraySecond) {
+        $result = ['key' => $key];
+        if (bothHaveArraysByKey($arrayFirst, $arraySecond, $key)) {
+            $result['children'] = createDiff($arrayFirst[$key], $arraySecond[$key]);
+            return $result;
+        }
+
+        if (array_key_exists($key, $arrayFirst)) {
+            $result['valueBefore'] = $arrayFirst[$key];
+        }
+        if (array_key_exists($key, $arraySecond)) {
+            $result['valueAfter'] = $arraySecond[$key];
+        }
+
+        return $result;
+    }, $keys);
+}
+
 function genDiff(string $pathToFile1, string $pathToFile2)
 {
     $arrayBefore = readFile($pathToFile1);
     $arrayAfter = readFile($pathToFile2);
 
-    $keys = mergeKeys($arrayBefore, $arrayAfter);
+    return createDiff($arrayBefore, $arrayAfter);
+}
 
-    $result = '';
-    foreach ($keys as $key) {
-        $result .= compareArrays($arrayBefore, $arrayAfter, $key);
-    }
+/**
+ * Interface
+ */
+function getKey($item)
+{
+    return $item['key'];
+}
 
-    return "{\n" . $result . "}\n";
+function hasChildren($item)
+{
+    return array_key_exists('children', $item);
+}
+
+function getChildren($item)
+{
+    return $item['children'];
+}
+
+function isTheSame($item)
+{
+    return array_key_exists('valueBefore', $item)
+        && array_key_exists('valueAfter', $item)
+        && $item['valueBefore'] === $item['valueAfter'];
+}
+
+function isChanged($item)
+{
+    return array_key_exists('valueBefore', $item)
+        && array_key_exists('valueAfter', $item)
+        && $item['valueBefore'] !== $item['valueAfter'];
+}
+
+function isDeleted($item)
+{
+    return array_key_exists('valueBefore', $item)
+        && !array_key_exists('valueAfter', $item);
+}
+
+function isAdded($item)
+{
+    return !array_key_exists('valueBefore', $item)
+        && array_key_exists('valueAfter', $item);
+}
+
+function getValueBefore($item)
+{
+    return $item['valueBefore'];
+}
+
+function getValueAfter($item)
+{
+    return $item['valueAfter'];
 }
