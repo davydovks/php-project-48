d4b206886a9df236d2d613d67a890a5edae9962f
Konstantin Davydov <davydovks@mail.ru>
2023-06-26 16:06:46 +0500

    arrange functions in Plain.php in order

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -11,45 +11,6 @@ use function Differ\Differ\isAdded;
 use function Differ\Differ\getValueBefore;
 use function Differ\Differ\getValueAfter;
 
-function toStringPlain(mixed $value)
-{
-    if (is_array($value)) {
-        return '[complex value]';
-    }
-
-    if (is_string($value)) {
-        return var_export($value, true);
-    }
-
-    return json_encode($value);
-}
-
-function getProperty(array $stack)
-{
-    return implode('.', $stack);
-}
-
-function getLineChanged(array $item, array $stack)
-{
-    $property = getProperty($stack);
-    $old = toStringPlain(getValueBefore($item));
-    $new = toStringPlain(getValueAfter($item));
-    return "Property '{$property}' was updated. From {$old} to {$new}";
-}
-
-function getLineAdded(array $item, array $stack)
-{
-    $property = getProperty($stack);
-    $value = toStringPlain(getValueAfter($item));
-    return "Property '{$property}' was added with value: {$value}";
-}
-
-function getLineRemoved(array $stack)
-{
-    $property = getProperty($stack);
-    return "Property '{$property}' was removed";
-}
-
 function formatDiff(array $diff): string
 {
     $iter = function ($coll, array $parentElem) use (&$iter) {
@@ -83,4 +44,41 @@ function formatDiff(array $diff): string
     $lines = $iter($diff, []);
 
     return implode(PHP_EOL, $lines);
+}
+
+function getLineChanged(array $item, array $stack)
+{
+    $property = getProperty($stack);
+    $old = toStringPlain(getValueBefore($item));
+    $new = toStringPlain(getValueAfter($item));
+    return "Property '{$property}' was updated. From {$old} to {$new}";
+}
+
+function getProperty(array $stack)
+{
+    return implode('.', $stack);
+}
+
+function toStringPlain(mixed $value)
+{
+    if (is_array($value)) {
+        return '[complex value]';
+    } elseif (is_string($value)) {
+        return var_export($value, true);
+    }
+
+    return json_encode($value);
+}
+
+function getLineAdded(array $item, array $stack)
+{
+    $property = getProperty($stack);
+    $value = toStringPlain(getValueAfter($item));
+    return "Property '{$property}' was added with value: {$value}";
+}
+
+function getLineRemoved(array $stack)
+{
+    $property = getProperty($stack);
+    return "Property '{$property}' was removed";
 }
